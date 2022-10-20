fd978e2c39d6924234a3ac39e0970def30a3184c
lsudupe <sudupelaura@gmail.com>
2022-10-20 14:12:05 +0300

    actualization, endogram no so good idea while working with so many points

--- endogram.R
+++ endogram.R
@@ -4,6 +4,7 @@
 
 #Libraries---------------------------------
 library(Seurat)
+library(ggplot2)
 #BiocManager::install("HGC")
 library(HGC)
 #https://www.datanovia.com/en/lessons/examples-of-dendrograms-visualization/
@@ -20,18 +21,34 @@ Idents(integrated_seurat) <- integrated_seurat@meta.data[["area"]]
 integrated_seurat <- FindClusteringTree(integrated_seurat, graph.type = "KNN")
 seurat_tree <- integrated_seurat@graphs$ClusteringTree
 seurat_data <- as.data.frame(integrated_seurat@meta.data)
-seurat.labels <- data.frame(seurat_cluster = seurat_data$seurat_clusters ,
-                            area = seurat_data$area)
+seurat.labels <- data.frame(area = seurat_data$area,
+                            prueba = seurat_data$orig.ident)
 
 seurat_tree$height = log(seurat_tree$height + 1)
 seurat_tree$height = log(seurat_tree$height + 1)
 
 seurat_tree[["labels"]] <- integrated_harmony@meta.data[["area"]]
-pdf(file.path("./results/endogram/",filename = "seurat.pdf"))
+
+pdf(file.path("./results/endogram/",filename = "endo.pdf"))
 print(HGC.PlotDendrogram(tree = seurat_tree,
                          k = 5, plot.label = TRUE,
                          labels = seurat.labels))
 dev.off()
+
+pdf(file.path("./results/endogram/",filename = "seurat_spatial.pdf"))
+print(SpatialDimPlot(integrated_seurat, label = T, combine = F))
+dev.off()
+
+a <- HGC.PlotDendrogram(tree = seurat_tree,
+                        k = 5, plot.label = TRUE,
+                        labels = seurat.labels)
+library(plotly)
+library(ggplot2)
+library(ggdendro)
+
+p <- ggdendrogram(seurat_tree, rotate = FALSE, size = 2)
+print(ggplotly(p)) 
+
 
 library(BuenColors)
 nuria <- jdb_palette("brewer_spectra")
