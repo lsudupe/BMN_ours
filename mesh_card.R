ede73ebe7d06348ccdced0a191f533042a43f768
lsudupe <sudupelaura@gmail.com>
2022-11-15 18:20:39 +0300

    small changes

--- mesh_card.R
+++ mesh_card.R
@@ -20,8 +20,19 @@ source(file = "./card.plot2.R")
 spatial <- readRDS("./objects/sp/second/combined_filtered.rds")
 single_cell_bonemarrow <- readRDS("./objects/sc/single_cell_bonemarrow.rds")
 
+## Spatial object
+M1_fem_1C <- readRDS("./objects/card/M1_fem_1C_subgroup.rds")
+M1_tib_1A <- readRDS("./objects/card/M1_tib_1A_subgroup.rds")
+M3_fem_1C <- readRDS("./objects/card/M3_fem_1C_subgroup.rds")
+M3_tib_2A <- readRDS("./objects/card/M3_tib_2A_subgroup.rds")
 
-CARD_obj = CARD.imputation(M3_tib_2A_CARD_obj,NumGrids = 2000,ineibor = 10,exclude = NULL)
+## Card object
+M1_fem_1C_CARD_obj <- readRDS("./objects/card/M1_fem_1C_CARD_obj_subgroup.rds")
+M1_tib_1A_CARD_obj <- readRDS("./objects/card/M1_tib_1A_CARD_obj_subgroup.rds")
+M3_fem_1C_CARD_obj <- readRDS("./objects/card/M3_fem_1C_CARD_obj_subgroup.rds")
+M3_tib_2A_CARD_obj <- readRDS("./objects/card/M3_tib_2A_CARD_obj_subgroup.rds")
+
+CARD_obj = CARD.imputation(M1_fem_1C_CARD_obj,NumGrids = 375,ineibor = 10,exclude = NULL)
 
 ## Visualize the newly grided spatial locations to see if the shape is correctly detected. If not, the user can provide the row names of the excluded spatial location data into the CARD.imputation function
 location_imputation = cbind.data.frame(x=as.numeric(sapply(strsplit(rownames(CARD_obj@refined_prop),split="x"),"[",1)),
@@ -37,15 +48,39 @@ p4 <- ggplot(location_imputation,
         panel.border = element_rect(colour = "grey89", fill=NA, size=0.5))
 print(p4)
 
-sub_list <- as.vector(levels(single_cell_bonemarrow@meta.data[["groups"]]))
- 
+sub_list <- as.vector(levels(single_cell_bonemarrow@meta.data[["cell"]]))
+
+
 p5 <- CARD.visualize.prop.2(
   proportion = CARD_obj@refined_prop,                         
   spatial_location = location_imputation,            
   ct.visualize = sub_list,                    
   colors = c("lightblue","lightyellow","red"),    
-  NumCols = 7)          
-
-
-
+  NumCols = 7)
+pdf(file.path("./results/CARD/mesh/sub_groups/",filename = ("Smesh.pdf")))
 print(p5)
+dev.off()
+
+#####extract refined proportions
+#refined_prop <- CARD_obj@refined_prop
+#img <- GetImage(M3_tib_2A)
+#coords <- GetTissueCoordinates(object = M3_tib_2A[[img]])
+vv <- as.data.frame(CARD_obj@algorithm_matrix[["Res"]][["V"]])
+vvv <- colnames(vv)
+M1_fem_1C@meta.data[["Sinusoidal_ECs"]] <- vv[,20]
+
+pdf(file.path("./results/CARD/mesh/sub_groups/",filename = ("Sinusoidal_ECs.pdf")))
+print(SpatialFeaturePlot(M1_fem_1C, features = c("Sinusoidal_ECs"), pt.size.factor = 7, alpha = 0.6))
+dev.off()
+
+
+p1 <- SpatialFeaturePlot(fibro.new, features = i, combine = FALSE)
+fix.p1 <- scale_fill_gradientn(colours=c(bl,"white", re), na.value = "grey98",limits = b,breaks=b, labels = c("min", "max"))
+#fix.p1 <- scale_fill_gradientn(colours=myPalette(100),breaks=b, labels = c("min", "max"),limits = b)
+p2 <- lapply(p1, function (x) x + fix.p1)
+
+pdf(file.path("./results/Aucell/B_genes/",filename = paste(i,"spatial.b.pdf",sep="")))
+print(CombinePlots(p2))
+dev.off()
+
+
