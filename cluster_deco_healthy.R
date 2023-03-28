3a77eb169739654bf26352d49d7bc0fc27d4669a
lsudupe <sudupelaura@gmail.com>
2023-03-28 18:39:35 +0300

    changes

--- cluster_deco_healthy.R
+++ cluster_deco_healthy.R
@@ -105,6 +105,8 @@ dev.off()
 meta_b <- b@meta.data
 types_b <- meta_b[,12:20]
 types_b["clustering"] <- as.vector(b@meta.data[["clustering"]])
+types_b["sample"] <- as.vector(b@meta.data[["name"]])
+
 
 ##distribution plot
 pdf(file.path("./results/endogram/st/healthy/",filename = "clustering_distribution.pdf"))
@@ -114,41 +116,18 @@ types_b %>%
   ggplot(aes(x = name, y = value, fill = Cluster)) + geom_violin()
 dev.off()
 
-#####correlation per cluster
-clust_1 <- types_b[grepl("1", types_b$clustering),]
-clust_1$clustering <- NULL
-clust_2 <- types_b[grepl("2", types_b$clustering),]
-clust_2$clustering <- NULL
-clust_3 <- types_b[grepl("3", types_b$clustering),]
-clust_3$clustering <- NULL
-clust_4 <- types_b[grepl("4", types_b$clustering),]
-clust_4$clustering <- NULL
-clust_5 <- types_b[grepl("5", types_b$clustering),]
-clust_5$clustering <- NULL
-clust_6 <- types_b[grepl("6", types_b$clustering),]
-clust_6$clustering <- NULL
+types_only <- types_b
+types_only$clustering <- NULL
 
-###keep healthy
-types_b["sample"] <- as.vector(b@meta.data[["name"]])
-healthy <- types_b[!grepl("M1_fem_1C", types_b$sample),]
-healthy <- healthy[!grepl("M2_F_2B", healthy$sample),]
-healthy <- healthy[!grepl("M8_F2_1C", healthy$sample),]
-healthy <- healthy[!grepl("M9_F2_1C", healthy$sample),]
-healthy$sample <- NULL
-
-matrix <- data.matrix(healthy, rownames.force = NA)
+matrix <- data.matrix(types_only, rownames.force = NA)
 M <- cor(matrix)
 
-pdf(file.path("./results/endogram/st/",filename = "healthy_cor.pdf"))
-print(corrplot(M,
-               type = 'upper',
-               title="healthy cell types correlation",
-               tl.col = "black",number.cex = 0.75,
-               mar=c(0,0,1,0)))
+pdf(file.path("./results/endogram/st/healthy/",filename = "healthy_cor.pdf"))
+print(corrplot(M, method = 'square', order = 'FPC', type = 'lower', diag = FALSE))
 dev.off()
 
 
-##cor
+##cor BY CLUSTER
 matrix <- data.matrix(clust_6, rownames.force = NA)
 M <- cor(matrix)
 
@@ -158,40 +137,31 @@ print(corrplot(M,
                tl.col = "black",number.cex = 0.75))
 dev.off()
 
-##coor all
-se.subset <- SubsetSTData(se, features = keep.features)
-
-pdf(file.path("./results/endogram/st/",filename = "cluster6_cor.pdf"))
-print(corrplot(M,
-               type = 'upper',
-               tl.col = "black",number.cex = 0.75))
-dev.off()
-
+##cor BY CLUSTER FIN
 
 ####Extract hierarchycal clustering porcentages
 #######proportions loop
 value <- as.vector(unique(types_b$clustering))
-value <- as.vector(unique(types_b$sample))
+#value <- as.vector(unique(types_b$sample))
 
 lista <- list()
 
 for (i in value){
 #select cluster of interest rows
-value_1 <- types_b[grepl(i, types_b$sample),]
-value_1$sample <- NULL
+value_1 <- types_b[grepl(i, types_b$clustering),]
+value_1$clustering <- NULL
 
 ###create list to add content
 proportions <- c()
   for (o in colnames(value_1)){ 
     proportions <- c(proportions, (sum(value_1[[o]])*100/nrow(value_1)))
   }
-name <- paste('sample:',i,sep='')
+name <- paste('cluster:',i,sep='')
 lista[[name]] <- proportions
 
 }
 
 ######create a df 
-rows = colnames(value_1)
 df = data.frame(matrix(nrow = length(colnames(value_1)), ncol = 0)) 
 rownames(df) = colnames(value_1)
 
@@ -204,16 +174,22 @@ for (i in 1:length(lista)){
 
 write.csv(df, "./celltypes.csv", row.names=TRUE)
 
-df["celltype"] <- as.vector(rownames(df))
-
+#df["celltype"] <- as.vector(rownames(df))
 
 # Basic piechart
-pdf(file.path("./results/endogram/st",filename = "clustering_percentages_piechart.pdf"))
-ggplot(df, aes(x="", y=df$`sample:M1_fem_1C`, fill=df$celltype)) +
+pdf(file.path("./results/endogram/st/healthy/",filename = "clustering_percentages_piechart_cluster1.pdf"))
+ggplot(df, aes(x="", y=df$`cluster:1`, fill=df$celltype)) +
   geom_bar(stat="identity", width=1) +
-  geom_text(aes(label = df$`sample:M1_fem_1C`),
+  geom_text(aes(label = df$`cluster:1`),
             position = position_stack(vjust = 0.5)) +
   coord_polar("y", start=0)
+dev.off()
+
+
+# Stacked + percent
+pdf(file.path("./results/endogram/st/healthy/",filename = "clustering_percentages_barplot_onlyhealthy.pdf"))
+ggplot(df, aes(fill=group, y=Value, x=Cluster)) + 
+  geom_bar(position="fill", stat="identity")
 dev.off()
 
 
@@ -227,31 +203,91 @@ DF <- data.frame(group = c(df$celltype),
                  cluster1 = c(df$`cluster:1`),
                  cluster2 = c(df$`cluster:2`),
                  cluster3 = c(df$`cluster:3`),
-                 cluster4 = c(df$`cluster:4`),
-                 cluster5 = c(df$`cluster:5`),
-                 cluster6 = c(df$`cluster:6`)
+                 cluster4 = c(df$`cluster:4`)
                  )
-DFtall <- DF %>% gather(key = Cluster, value = Value, cluster1:cluster6)
+DFtall <- DF %>% gather(key = Cluster, value = Value, cluster1:cluster4)
 DFtall
 
-pdf(file.path("./results/endogram/st",filename = "clustering_percentages.pdf"))
+pdf(file.path("./results/endogram/st/healthy/",filename = "clustering_percentages.pdf"))
 ggplot(DFtall, aes(Cluster, Value, fill = group)) + geom_col(position = "dodge")
 dev.off()
 
 # Stacked + percent
-pdf(file.path("./results/endogram/st",filename = "clustering_percentages_barplot.pdf"))
+pdf(file.path("./results/endogram/st/healthy/",filename = "clustering_percentages_barplot_onlyhealthy.pdf"))
 ggplot(DFtall, aes(fill=group, y=Value, x=Cluster)) + 
   geom_bar(position="fill", stat="identity")
 dev.off()
 
-##borrar no healthy
-new <- DFtall[!grepl("cluster5", DFtall$Cluster),]
-new <- new[!grepl("cluster6", new$Cluster),]
+
+######VIOLIN PLOT 
+
+se <- readRDS("./objects/heterogeneity/healthy/se_hierarchical.rds")
+b <- SetIdent(se, value = se@meta.data[["clustering"]])
+###subset the data in hierarchical clustering
+meta_b <- b@meta.data
+types_b <- meta_b[,12:20]
+types_b["sample"] <- as.vector(b@meta.data[["name"]])
+
+##distribution plot
+pdf(file.path("./results/endogram/st/healthy/",filename = "sample_distribution.pdf"))
+types_b %>%
+  pivot_longer(cols = -sample) %>%
+  mutate(Sample = factor(sample)) %>%
+  ggplot(aes(x = name, y = value, fill = Sample)) + geom_violin()
+dev.off()
+
+value <- as.vector(unique(types_b$sample))
+
+lista <- list()
+for (i in value){
+  #select cluster of interest rows
+  value_1 <- types_b[grepl(i, types_b$sample),]
+  value_1$sample <- NULL
+  
+  ###create list to add content
+  proportions <- c()
+  for (o in colnames(value_1)){ 
+    proportions <- c(proportions, (sum(value_1[[o]])*100/nrow(value_1)))
+  }
+  name <- paste('sample:',i,sep='')
+  lista[[name]] <- proportions
+}
+
+
+######create a df 
+df = data.frame(matrix(nrow = length(colnames(value_1)), ncol = 0)) 
+rownames(df) = colnames(value_1)
+
+##add list values to df
+for (i in 1:length(lista)){
+  a <- lista[[i]]
+  df[, ncol(df) + 1] <- a
+  names(df)[ncol(df)] <- names(lista[i])
+}
+
+
+#df <- t(df)
+df <- cbind(celltype = rownames(df), df)
+rownames(df) <- 1:nrow(df)
+
+library(tidyr)
+library(ggplot2)
+DF <- data.frame(group = c(df$celltype),
+                 M3_F_1C = c(df$`sample:M3_F_1C`),
+                 M3_fem_1C = c(df$`sample:M3_fem_1C`)
+)
+DFtall <- DF %>% gather(key = sample, value = Value, M3_F_1C:M3_fem_1C)
+DFtall
+
 
 # Stacked + percent
-pdf(file.path("./results/endogram/st",filename = "clustering_percentages_barplot_onlyhealthy.pdf"))
-ggplot(new, aes(fill=group, y=Value, x=Cluster)) + 
+pdf(file.path("./results/endogram/st/healthy/",filename = "sample_percentages_barplot.pdf"))
+ggplot(DFtall, aes(fill=group, y=Value, x=sample)) + 
   geom_bar(position="fill", stat="identity")
 dev.off()
+
+
+
+######VIOLIN PLOT FIN
 
 
