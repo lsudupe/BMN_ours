bb9861e6e20568e531b61fec5182436edaa3439a
lsudupe <sudupelaura@gmail.com>
2023-04-11 16:27:45 +0300

    add cell type per cluster and different genes plots

--- gradients_st.R
+++ gradients_st.R
@@ -13,10 +13,57 @@ library(ggthemes)
 library(viridis)
 
 
-
-
 #Data---------------------------------
 se <- readRDS("./objects/sc/integrated/se_deco.rds")
+se <- readRDS("./objects/heterogeneity/se_hierarchical.rds")
+
+########cell types driving the cluster
+x <- se
+
+##replace all values below 10% with 0
+meta <- x@meta.data
+a <- meta[,12:21]
+a["cluster"] <- as.vector(x@meta.data[["clustering"]])
+
+df <- a
+# Filter values below 10%
+
+# Calculate the 75th percentile threshold for each row
+thresholds <- apply(df[, -ncol(df)], 1, function(x) quantile(x, 0.75))
+
+# Filter values below the 75th percentile threshold for each row
+df_filtered <- df %>%
+  mutate(across(-cluster, ~ifelse(. < thresholds[row_number()], 0, .)))
+
+# Filter values below 10%
+df_filtered <- df %>%
+  mutate(across(-cluster, ~ifelse(. < 0.15, 0, .)))
+
+# Reshape the data frame to a long format for plotting
+df_long <- df_filtered %>%
+  pivot_longer(cols = -cluster,
+               names_to = "cell_type",
+               values_to = "percentage") %>%
+  filter(percentage > 0)
+
+# Create a plot to visualize the number of major cell types driving each cluster
+plot <- ggplot(df_long, aes(x = cluster, y = cell_type)) +
+  geom_count(aes(size = ..n.., color = cell_type)) +
+  labs(title = "Major Cell Types Driving Each Cluster",
+       x = "Cluster",
+       y = "Cell Type",
+       size = "Count") +
+  theme_minimal() +
+  scale_color_discrete(name = "Cell Type")
+
+# Print the plot
+pdf(file.path("./results/ST/gradient/cell_types_per_cluster_15percent.pdf"))
+print(plot)
+dev.off()
+
+
+########
+
 
 library(RColorBrewer)
 display.brewer.all()
@@ -38,26 +85,47 @@ print(p)
 dev.off()
 
 ####marker genes https://www.nature.com/articles/s41467-022-33944-z
-
+#CD81, XBP1, MKI67, PCNA, FLNA, CD44
 ###ANORMAL plasma cells
 p <-FeatureOverlay(se, features = c("Xbp1"), sampleids = 1:6,pt.size = 0.7,ncol = 2 , 
-                   value.scale = "all" ,cols = c("lightgray", "mistyrose", "red", "dark red", "black"))
+                   value.scale = "all" ,cols = color)
 
 pdf(file.path("./results/ST/gradient/pc_markers/Xbp1.pdf"))
 print(p)
 dev.off()
 
-p <-FeatureOverlay(se, features = c("Irf4"), sampleids = 1:6,pt.size = 0.7,ncol = 2 , 
-                   value.scale = "all" ,cols = c("lightgray", "mistyrose", "red", "dark red", "black"))
+p <-FeatureOverlay(se, features = c("Cd81"), sampleids = 1:6,pt.size = 0.7,ncol = 2 , 
+                   value.scale = "all" ,cols = color)
 
-pdf(file.path("./results/ST/gradient/pc_markers/Irf4.pdf"))
+pdf(file.path("./results/ST/gradient/pc_markers/Cd81.pdf"))
 print(p)
 dev.off()
 
-p <-FeatureOverlay(se, features = c("Itgb2"), sampleids = 1:6,pt.size = 0.7,ncol = 2 , 
-                   value.scale = "all" ,cols = c("lightgray", "mistyrose", "red", "dark red", "black"))
+p <-FeatureOverlay(se, features = c("Mki67"), sampleids = 1:6,pt.size = 0.7,ncol = 2 , 
+                   value.scale = "all" ,cols = color)
 
-pdf(file.path("./results/ST/gradient/pc_markers/Itgb2.pdf"))
+pdf(file.path("./results/ST/gradient/pc_markers/Mki67.pdf"))
+print(p)
+dev.off()
+
+p <-FeatureOverlay(se, features = c("Pcna"), sampleids = 1:6,pt.size = 0.7,ncol = 2 , 
+                   value.scale = "all" ,cols = color)
+
+pdf(file.path("./results/ST/gradient/pc_markers/Pcna.pdf"))
+print(p)
+dev.off()
+
+p <-FeatureOverlay(se, features = c("Flna"), sampleids = 1:6,pt.size = 0.7,ncol = 2 , 
+                   value.scale = "all" ,cols = color)
+
+pdf(file.path("./results/ST/gradient/pc_markers/Flna.pdf"))
+print(p)
+dev.off()
+
+p <-FeatureOverlay(se, features = c("Cd44"), sampleids = 1:6,pt.size = 0.7,ncol = 2 , 
+                   value.scale = "all" ,cols = color)
+
+pdf(file.path("./results/ST/gradient/pc_markers/Cd44.pdf"))
 print(p)
 dev.off()
 
