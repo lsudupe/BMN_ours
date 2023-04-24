cb6464f1de6873426d232132378a634a4262db3f
lsudupe <sudupelaura@gmail.com>
2023-04-24 12:53:31 +0300

    add boxplots

--- canonical_st.R
+++ canonical_st.R
@@ -9,6 +9,7 @@ library(STutility)
 
 #Data---------------------------------
 objects <- readRDS("./objects/sp/regress_out/list_regressout_ST.rds")
+all <- readRDS("./objects/heterogeneity/se_hierarchical.rds")
 
 names <- names(objects)
 #Check dormant enrichment score
@@ -18,23 +19,21 @@ display.brewer.all()
 color <- brewer.pal(11,"Spectral")
 color <- rev(color)
 
+#set colors
+n = 7
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+cols = gg_color_hue(n)
+cluster_color_map <- setNames(cols, unique(all@meta.data[["clustering"]]))
+
 dormant <- c("C1qa", "Aif1" ,"Axl" ,"II18bp", "Glul", "Mpeg1", "H2-Eb1",
              "Nr1h3" ,"Lilrb4" ,"Sdc3" ,"Fcgr3" ,"Oas1a", "Ifit2", "Ly6a",
              "Ly86" ,"Bcl2a1b", "Cd19" ,"Gm4955", "Hebp1", "Sirpa" ,"Slfn2" ,"Tnfaip2" ,
              "Vpreb3", "ligp1", "Oasl2","am213b" ,"Slc43a2", "Ifi44", "Gng10" ,"Cxcl16" ,"Ptprcap" ,
              "Anxa2" ,"Rgs2" ,"Tmed3" ,"Igll1", "Hpgd", "Glipr1", "Cd4" ,"Cd84" ,"Gbp2", "AB124611", "Slc44a2" ,
              "Samd9l" ,"Oas1g" ,"Fcgr1", "Pla2g15", "Tifa" ,"Pmp22", "Abcc3" ,"S100a10")
-
-new <- c()
-for (i in 1:length(objects)){
-  a <- objects[[i]]
-  b <- names(objects[i])
-  a <- AddModuleScore(a,
-                      features = list(dormant),
-                      name="dormant")
-  ## add object to list
-  new[[length(new) + 1]] <- a
-}
 
 names(new) <- names
 
@@ -68,7 +67,58 @@ print(FeatureOverlay(new[["M9_F2_1C"]], features = c("dormant1"), pt.size = 1.8,
                value.scale = "all" ,cols = color))
 dev.off()
 
+####INFOR
+enrichment.meta <- as.vector(enrichment.meta$Enrichment)
 
+dpi3@meta.data["segmen"] <- as.factor(enrichment.meta)
+dpi3@meta.data[["segmen"]] <- factor(dpi3@meta.data[["segmen"]],
+                                     levels = c("IZ","BZ_1","BZ_2_3","BZ_4","RZ"),ordered = TRUE)
+#Create a custom color scale
+library(RColorBrewer)
+nombres <- c("IZ","BZ_1","BZ_2","BZ_3","BZ_2_3","BZ_4","RZ")
+colors <- c("#000033", "#006666", "#66CCCC", "#99FFCC", "#CCCC33", "#99CC99","#FFFF00")
+names(colors)  <- nombres
+
+#
+
+new <- c()
+for (i in 1:length(objects)){
+  a <- objects[[i]]
+  b <- names(objects[i])
+  a <- AddModuleScore(a,
+                      features = list(dormant),
+                      name="dormant")
+  ## add object to list
+  new[[length(new) + 1]] <- a
+}
+
+
+####INFOR FIN
+
+
+####Segment plots
+pdf(file.path("./results/ST/dormant/M9_F2_1C_boxplot.pdf"))
+new[["M9_F2_1C"]]@meta.data%>% 
+  ggplot(aes(x=dormant1, y= clustering, fill=clustering)) + 
+  geom_boxplot(aes(fill=clustering)) +  
+  scale_fill_manual(values =cluster_color_map ) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  #xlim(-0.5, 0.5) +
+  theme(plot.title = element_text(hjust=0.5, face="bold")) 
+dev.off()
+pdf(file.path("./results/ST/dormant/M8_F2_1C_boxplot.pdf"))
+new[["M8_F2_1C"]]@meta.data%>% 
+  ggplot(aes(x=dormant1, y= clustering, fill=clustering)) + 
+  geom_boxplot(aes(fill=clustering)) +  
+  scale_fill_manual(values =cluster_color_map ) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  #xlim(-0.5, 0.5) +
+  theme(plot.title = element_text(hjust=0.5, face="bold")) 
+dev.off()
+
+####Segment plots FIN
 
 ##Plots
 custom_theme <- theme(legend.position = c(0.45, 0.8), # Move color legend to top
