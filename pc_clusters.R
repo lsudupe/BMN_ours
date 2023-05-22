f5ea5ec9b941dfb1e37c98cc20b3db4259dc1e5a
lsudupe <sudupelaura@gmail.com>
2023-05-22 10:46:07 +0200

    label the hotspots correctly

--- pc_clusters.R
+++ pc_clusters.R
@@ -19,7 +19,13 @@ M8@meta.data <- meta
 meta <- meta[, ncol(meta), drop = FALSE]
 
 write.csv(meta, "./data/areas/m8_clustering.csv", row.names=TRUE)
-M8_s <- ManualAnnotation(M8)
+#M8_s <- ManualAnnotation(M8)
+meta <- M8_s@meta.data
+meta <- meta %>%
+  mutate(pc_clusters = paste0("M8_", labels, "_cluster", clustering))
+M8_s@meta.data <- meta
+saveRDS(M8_s, "./objects/pc_clusters/M8_s.rds")
+
 #M2
 M2 <- SubsetSTData(se, ident = "M2_F_2B")
 meta <- M2@meta.data
@@ -28,4 +34,53 @@ M2@meta.data <- meta
 meta <- meta[, ncol(meta), drop = FALSE]
 
 write.csv(meta, "./data/areas/m2_clustering.csv", row.names=TRUE)
-M2_s <- ManualAnnotation(M2)
+#M2_s <- ManualAnnotation(M2)
+meta <- M2_s@meta.data
+meta <- meta %>%
+  mutate(pc_clusters = paste0("M2_", labels, "_cluster", clustering))
+M2_s@meta.data <- meta
+saveRDS(M2_s, "./objects/pc_clusters/M2_s.rds")
+
+#M9
+M9 <- SubsetSTData(se, ident = "M9_F2_1C")
+meta <- M9@meta.data
+rownames(meta) <- sub("_5", "", rownames(meta))
+M9@meta.data <- meta
+meta <- meta[, ncol(meta), drop = FALSE]
+
+write.csv(meta, "./data/areas/m9_clustering.csv", row.names=TRUE)
+M9_s <- ManualAnnotation(M9)
+meta <- M9_s@meta.data
+meta <- meta %>%
+  mutate(pc_clusters = paste0("M9_", labels, "_cluster", clustering))
+M9_s@meta.data <- meta
+saveRDS(M9_s, "./objects/pc_clusters/M9_s.rds")
+
+#M1
+M1 <- SubsetSTData(se, ident = "M1_fem_1C")
+meta <- M1@meta.data
+rownames(meta) <- sub("_5", "", rownames(meta))
+M1@meta.data <- meta
+meta <- meta[, ncol(meta), drop = FALSE]
+
+write.csv(meta, "./data/areas/m1_clustering.csv", row.names=TRUE)
+#M1_s <- ManualAnnotation(M1)
+meta <- M1_s@meta.data
+meta <- meta %>%
+  mutate(pc_clusters = paste0("M1_", labels, "_cluster", clustering))
+M1_s@meta.data <- meta
+saveRDS(M1_s, "./objects/pc_clusters/M1_s.rds")
+
+###combine the data
+
+se_s <- MergeSTData(M1_s, y = c(M2_s, M8_s, M9_s), 
+                  add.spot.ids = c("M1_s", "M2_s", "M8_s", "M9_s"), project = "BM")
+
+saveRDS(se_s, "./objects/pc_clusters/combined_s.rds")
+
+################ CHECK DORMANT SIGNATURE
+
+
+
+
+
