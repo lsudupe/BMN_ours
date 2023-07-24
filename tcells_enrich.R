c45a2c91a899fb93dd87a9379bbd76d0b73312ee
lsudupe <sudupelaura@gmail.com>
2023-07-24 11:28:59 +0200

    try new data

--- tcells_enrich.R
+++ tcells_enrich.R
@@ -24,6 +24,12 @@ tcells16 <- c("Cd8a", "Cd4", "Foxp3", "Il2ra", "Pdcd1", "Lag3", "Tigit", "Ifng",
 
 tcells8 <- c("Cd4", "Foxp3", "Tigit", "Ctla4", "Cxcr3", "Tnfrsf9", "Icos", "Tnfrsf4")
 
+candidate <- c("Pdcd1", "Ctla4", "Tnfrsf9", "Havcr2", "Tox", "Tigit", "Wars", "Rsad2",
+               "Mcm7", "Mx1", "Ndfip2", "Enosf1", "Ccdc141", "Stmn1", "Ttn", "Faslg",
+               "Mcm5", "Nab1", "Phlda1", "Mcm3", "Pcna", "Gapdh", "Oasl", "Ifi44l",
+               "Tbc1d4", "Slc43a3", "Pam", "Ccl3", "Acp5", "Oas3", "Cd38", "Tnfs10",
+               "Gbp2", "Kif20b", "Ctsb")
+
 #CD45,CD56,CD117
 # Check which genes_to_check are in seurat_genes
 genes_present <- tcells8 %in% seurat_genes
@@ -31,6 +37,31 @@ genes_present <- tcells8 %in% seurat_genes
 
 ###score
 ## Add UCellScore
+#candidate
+vector<- ScoreSignatures_UCell(all@assays[["RNA"]]@counts, features = list(candidate))
+all@meta.data[["signature_1_candidate"]] <- as.vector(vector)
+
+p <- FeatureOverlay(all, features = c("signature_1_candidate"),sampleids = 1:6, ncols = 2, pt.size = 0.7, 
+                    value.scale = "all" ,cols = color)
+
+pdf(paste("./results/tcells/mouse/candidate_all.pdf",sep=""))
+print(p)
+dev.off()
+
+Idents(object = all) <- "clustering"
+plot <- VlnPlot(object = all, features = "signature_1_candidate")
+
+pdf(paste("./results/tcells/mouse/candidate_violin.pdf",sep=""), width = 10, height = 7)
+print(plot)
+dev.off()
+
+plot <- VlnPlot(all, "signature_1_candidate", group.by = "clustering", split.by = "clustering") +
+  geom_boxplot()
+
+pdf(paste("./results/tcells/mouse/candidate_violin.pdf",sep=""), width = 10, height = 7)
+print(plot)
+dev.off()
+
 #16genes
 vector<- ScoreSignatures_UCell(all@assays[["RNA"]]@counts, features = list(tcells16))
 all@meta.data[["signature_1_tcells"]] <- as.vector(vector)
@@ -58,6 +89,7 @@ p <- FeatureOverlay(all, features = c("signature_1_tcells8"),sampleids = 1:6, nc
 pdf(paste("./results/tcells/mouse/genes8_all.pdf",sep=""))
 print(p)
 dev.off()
+
 
 ##individual genes
 for (i in tcells8){
@@ -111,4 +143,58 @@ for (i in tcells8){
 
 Idents(object = all) <- "clustering"
 VlnPlot(object = all, features = c("Cd4"))
+
+specific_genes <- c("Lag3", "Tigit", "Cd69", "Cd83", "Btla", "Slamf6", 
+                    "Ctla4")
+
+#Cd43, Pd1, Tim3 no
+
+for (i in specific_genes){
+  a <- i
+  p <- FeatureOverlay(all, features = i,sampleids = 1:6, ncols = 2, pt.size = 0.7, 
+                      value.scale = "all" ,cols = color)
+  
+  pdf(paste("./results/tcells/mouse/specific_genes/", i,"_tcell.pdf",sep=""))
+  print(p)
+  dev.off()
+  
+  plot <- VlnPlot(object = all, features = i, split.by = "clustering")
+  
+  pdf(paste("./results/tcells/mouse//specific_genes/", i,"_tcell_violin.pdf",sep=""), width = 10, height = 7)
+  print(plot)
+  dev.off()
+  
+  Idents(object = all) <- "clustering"
+  plot <- VlnPlot(object = all, features = i)
+  
+  pdf(paste("./results/tcells/mouse/specific_genes/", i,"_tcell_violin_ALL.pdf",sep=""), width = 10, height = 7)
+  print(plot)
+  dev.off()
+  
+  #boxplot
+  # Fetch data for the current feature
+  data <- FetchData(all, vars = i)
+  
+  # Add cluster identities to the data
+  data$cluster <- all@meta.data[["clustering"]]
+  
+  # Convert to long format data frame for ggplot
+  data_long <- data %>% pivot_longer(cols = starts_with(i),
+                                     names_to = "Feature",
+                                     values_to = "Expression") %>% 
+    filter(Expression != 0)
+  
+  # Plot boxplot using ggplot2
+  p <- ggplot(data_long, aes(x = cluster, y = Expression, fill = cluster)) + 
+    geom_boxplot() +
+    labs(title = i) +
+    theme_minimal() +
+    xlab("Cluster") +
+    ylab("Expression Level")
+  
+  pdf(paste("./results/tcells/mouse/specific_genes/", i,"_tcell_boxplot.pdf",sep=""), width = 10, height = 7)
+  print(p)
+  dev.off()
+  
+}
 
