c7ebb7d3ceeb820d45096554630f934831b2031e
lsudupe <sudupelaura@gmail.com>
2023-11-12 13:49:01 +0300

    select only genes intersecting

--- signature_human.R
+++ signature_human.R
@@ -14,29 +14,52 @@ library(UCell)
 se <- readRDS("./objects/heterogeneity/se_hierarchical.rds")
 human <- readRDS("./objects/sp/human/human_combined.rds")
 
+# split human
+B08041 <- human[["BM_human_AP-B08041_"]]
+B08805 <- human[["BM_human_AP-B08805"]]
+B10395 <- human[["BM_B10395"]]
 
 Idents(se) <- "clustering"
 all_markers <- FindAllMarkers(se, min.pct = 0.25)
 
 top_markers_per_cluster <- all_markers %>% 
   group_by(cluster) %>% 
-  top_n(n = 25, wt = -log10(p_val_adj))
-
-write.csv(top_markers_per_cluster, file = "./25.csv", row.names = FALSE)
+  top_n(n = 50, wt = -log10(p_val_adj))
 
 split_data <- split(top_markers_per_cluster, top_markers_per_cluster$cluster)
 
-# extract genes per cluster
-for(i in 1:length(split_data)) {
-  cluster_name <- unique(split_data[[i]]$cluster)[1]
-  gene_list <- toupper(split_data[[i]]$gene)  # Convert gene names to uppercase
-  assign(paste0("cluster", cluster_name), gene_list)
+# Assume you have gene lists for each human sample
+genes_B08041 <- rownames(B08041)  # Extract gene names
+genes_B08805 <- rownames(B08805)
+genes_B10395 <- rownames(B10395)
+
+# Find common genes across all three samples
+common_genes <- Reduce(intersect, list(genes_B08041, genes_B08805, genes_B10395))
+
+# Function to get top genes ensuring they are in the common gene list
+get_top_genes <- function(cluster_data, common_genes, n = 25) {
+  # Convert gene names in the cluster data to uppercase
+  cluster_genes_upper <- toupper(cluster_data$gene)
+  # Ensure that common_genes are also in uppercase
+  common_genes_upper <- toupper(common_genes)
+  # Check for common genes in uppercase
+  common_cluster_genes <- cluster_genes_upper[cluster_genes_upper %in% common_genes_upper]
+  if(length(common_cluster_genes) >= n) {
+    return(head(common_cluster_genes, n))
+  } else {
+    # Select additional genes if fewer than n are common
+    additional_genes <- setdiff(cluster_genes_upper, common_cluster_genes)
+    total_genes <- c(common_cluster_genes, head(additional_genes, n - length(common_cluster_genes)))
+    return(total_genes)
+  }
 }
 
-# split human
-B08041 <- human[["BM_human_AP-B08041_"]]
-B08805 <- human[["BM_human_AP-B08805"]]
-B10395 <- human[["BM_B10395"]]
+# Your existing loop
+for(i in 1:length(split_data)) {
+  cluster_name <- unique(split_data[[i]]$cluster)[1]
+  top_genes <- get_top_genes(split_data[[i]], common_genes)
+  assign(paste0("cluster", cluster_name), top_genes)
+}
 
 lista <- c(B08041, B08805, B10395)
 names(lista) <- c("B08041","B08805", "B10395")
@@ -79,61 +102,14 @@ for (i in 1:length(lista)){
     dev.off()
   }
   
-  ##ratio
-  enrich7 <- a@meta.data[["signature_1_cluster7"]]
-  enrich4 <- a@meta.data[["signature_1_cluster4"]]
-  enrich1 <- a@meta.data[["signature_1_cluster1"]]
-  
-  #ratio 1 vs 7
-  ratio_cluster1vs7 <- log10(enrich1/enrich7)
-  is.na(ratio_cluster1vs7) <-sapply(ratio_cluster1vs7, is.infinite)
-  ratio_cluster1vs7[is.na(ratio_cluster1vs7)] = 0
-  a$ratio_cluster1vs7 <- ratio_cluster1vs7
-  
-  #plot
-  bl <- colorRampPalette(c("navy","royalblue","lightskyblue"))(200)                      
-  re <- colorRampPalette(c("mistyrose", "red2","darkred"))(200)
-  
-  l <- c(min(a@meta.data[["ratio_cluster1vs7"]]), max(a@meta.data[["ratio_cluster1vs7"]]))
-  p1 <- FeatureOverlay(a, features = c("ratio_cluster1vs7"), ncols = 2, pt.size = 1.1, 
-                       value.scale = "all" ,cols = color)
-  fix.p1 <- scale_fill_gradientn(colours=c(bl,"white", re),
-                                 breaks=l,
-                                 labels=c("Min","Max"),
-                                 na.value = "grey98",
-                                 limits = l)
-  p2 <- p1[[1]] + fix.p1
-  
-  pdf(paste0("./results/human/cluster_enrich/25_ratio/", b, "_", "ratio_cluster1vs7.pdf", sep = ""))
-  print(p2)
-  dev.off()
-  
-  #ratio 4 vs 7
-  ratio_cluster4vs7 <- log10(enrich4/enrich7)
-  is.na(ratio_cluster4vs7) <-sapply(ratio_cluster4vs7, is.infinite)
-  ratio_cluster4vs7[is.na(ratio_cluster4vs7)] = 0
-  a$ratio_cluster4vs7 <- ratio_cluster4vs7
-  
-  #plot
-  bl <- colorRampPalette(c("navy","royalblue","lightskyblue"))(200)                      
-  re <- colorRampPalette(c("mistyrose", "red2","darkred"))(200)
-  
-  l <- c(min(a@meta.data[["ratio_cluster4vs7"]]), max(a@meta.data[["ratio_cluster4vs7"]]))
-  p1 <- FeatureOverlay(a, features = c("ratio_cluster4vs7"), ncols = 2, pt.size = 1.1, 
-                           value.scale = "all" ,cols = color)
-  fix.p1 <- scale_fill_gradientn(colours=c(bl,"white", re),
-                                 breaks=l,
-                                 labels=c("Min","Max"),
-                                 na.value = "grey98",
-                                 limits = l)
-  p2 <- p1[[1]] + fix.p1
-  
-  pdf(paste0("./results/human/cluster_enrich/25_ratio/", b, "_", "ratio_cluster4vs7.pdf", sep = ""))
-  print(p2)
-  dev.off()
-  
+  #########
+  # Call the function for each comparison
+  plot_ratio(a, 1, 7, color, "./results/human/cluster_enrich/25_ratio/")
+  plot_ratio(a, 5, 7, color, "./results/human/cluster_enrich/25_ratio/")
+  plot_ratio(a, 6, 7, color, "./results/human/cluster_enrich/25_ratio/")
   
   post[[length(post) + 1]] <- a
 }
 
+names(post) <- c("B08041","B08805", "B10395")
 
