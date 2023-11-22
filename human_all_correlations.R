ec1224ce42723926bdff0a34beaf5c64a5561643
lsudupe <sudupelaura@gmail.com>
2023-11-22 11:34:40 +0300

    correlation plot actualization

--- human_all_correlations.R
+++ human_all_correlations.R
@@ -5,72 +5,144 @@
 #Libraries---------------------------------
 library(Seurat)
 library(ggplot2)
-
+library(UCell)
+library(STutility)
+library(RColorBrewer)
+library(cowplot)
+library(ggpubr)
+color <- rev(brewer.pal(11,"Spectral"))
 
 ## Data
 healthy <- readRDS("./objects/sp/integrated/healthy_clean.rds")
 mm <- readRDS("./objects/sp/human/human_combined.rds")
 
 all <- c(healthy, mm)
-
 ## Signatures
-Tcell35genes <- c("Pdcd1", "Ctla4", "Tnfrsf9", "Havcr2", "Tox", "Tigit", "Wars", "Rsad2",
-                  "Mcm7", "Mx1", "Ndfip2", "Enosf1", "Ccdc141", "Stmn1", "Ttn", "Faslg",
-                  "Mcm5", "Nab1", "Phlda1", "Mcm3", "Pcna", "Gapdh", "Oasl", "fi44l",
-                  "Tbc1d4", "Slc43a3", "Pam", "Ccl3", "Acp5", "Oas3", "Cd38", "Tnfs10",
-                  "Gbp2", "Kif20b", "Ctsb")
-Tcell_exh_Isa_human <- c("HAVCR2","CTLA4","PDCD1","LAG3", "LAYN", "PD1", "TIGIT", "TIM3")
-cytotoxicity_human <- c("NKG7", "CCL4", "CST7", "PRF1", "GZMA", "GZMB", "IFNG", "CCL3")
-Teff_Juanro_human <- c("ALOX5AP","ANXA1","ARL4C","ATP6V1G1","B2M","BIN2","CD3D","CD48","CD52","CTSC","CTSW",
-                       "CXCR6","CYBA","EMP3","EOMES","EVL","FTL","FXYD5","GBP5","GRAP2","GZMA","GZMB","HCST",
-                       "IFITM2","IFNG","IL32","IL4R","IRF1","ISG15","ISG20","ITM2B","LGALS3","LST1","LTB",
-                       "MALAT1","MT-CO2","NCR3","PDLIM2","PRF1","RABAC1","RPL27A","RPS27","S100A4","S100A6",
-                       "SAMHD1","SDF2L1","SH3BGRL3","SLFN5","STK17B","TC2N","TMSB4X","TPT1","TRAC","XBP1")
-TcellAct_Juanro_human <- c("IFNG","CCL4","CCL3","IL3","XCL1","CSF2","GZMB","FABP5","XCL2","LTA","LAG3",
-                           "MIR155HG","TNFRSF4","TNFRSF9","PIM3","CIITA","CD74","HLA-DRB1","HLA-DRB5",
-                           "HLA-DMB","HLA-DPB1","HLA-DQA2","HLA-DOA","HLA-DRA","CD25","CD69","CCL5","SP140",
-                           "TIGIT","CD247","CCR5","NLRC3","PTPRCAP","STAT4","IL12RB1","TRAT1","SLA2","CXCR3",
-                           "ZAP70","NKG7","SIRPG","ICOS","IL18R1","SLAMF7","PTPN7","ITK","CRTAM","GZMA",
-                           "CD3E","GPR171","TARP","CD3D","LCK","SLAMF1","CD3G")
+source("./signatures.R")
+sig <- list(new_PC_markers_human, Teff_Juanro_human, TcellAct_Juanro_human, genes38_human, cytotoxicity_human)
+names(sig) <- c("new_PC_markers_human", "Teff_Juanro_human", "TcellAct_Juanro_human", "genes38_human", "cytotoxicity_human")
 
 ## Enrichment score
-for (i in 1:length(all)){
+post <- c()
+for (i in 1:length(all)) {
   a <- all[[i]]
-  b <- names(all[i])
+  b <- names(all)[i]
+  plots_list <- list()
   
-  for (cluster_name in cluster_names) {
-    # Define the variable names dynamically based on the cluster_name
-    signature_var <- paste0("signature_1_", cluster_name)
+  for (signature_name in 1:length(sig)) {
+    d <- names(sig[signature_name])
+    genes <- sig[[signature_name]]
     
-    file_name <- paste0("./results/human/cluster_enrich/new/25/", b, "_", cluster_name, "_pc.pdf", sep = "")
+    # Añade el UCellScore para la firma actual
+    vector <- ScoreSignatures_UCell(a@assays[["RNA"]]@counts, features = list(genes))
+    a@meta.data[[d]] <- as.vector(vector)
     
-    # Add UCellScore for the current cluster
-    vector <- ScoreSignatures_UCell(a@assays[["RNA"]]@counts, features = list(get(cluster_name)))
-    a@meta.data[[signature_var]] <- as.vector(vector)
+    # Aplicar subconjuntos si es necesario
+    # ... (tu código para aplicar subconjuntos) ...
     
-    #c <- c(min(a@meta.data[[signature_var]]), max(a@meta.data[[signature_var]]))
-    # Create the plot
-    p <- FeatureOverlay(a, features = c(signature_var), ncols = 1, pt.size = 1.1, 
-                        value.scale = "all" ,cols = color) +
-      scale_fill_gradientn(colours = color,
-                           breaks = c(0.0, 0.5),
-                           labels = c("Min", "Max"),
-                           limits = c(0.0, 0.5))
+    # Crea el gráfico
+    lim <- c(min(a@meta.data[[d]]), max(a@meta.data[[d]]))
+    p <- FeatureOverlay(a, features = c(d), ncols = 1, pt.size = 1.5, value.scale = "all", cols = color)
+    if (lim[1] != lim[2]) {
+      p <- p + scale_fill_gradientn(colours = color, breaks = lim, labels = lim, limits = lim)
+    }
     
-    # Save the plot to a PDF file
-    pdf(file_name)
-    print(p)
-    dev.off()
+    plots_list <- c(plots_list, list(p))
+  }
+  
+  # Combinar todos los gráficos en un único PDF para este objeto Seurat
+  file_name_all_plots <- paste0("./results/human/correlation/spatial/", b, "_all_plots.pdf")
+  if (length(plots_list) > 0) {
+    combined_plot <- ggarrange(plotlist = plots_list, ncol = 1, nrow = length(plots_list))
+    ggsave(file_name_all_plots, combined_plot, width = 11, height = 8.5 * length(plots_list), limitsize = FALSE)
+  } else {
+    message("No plots were created for ", b)
+  }
+  
+  post[[length(post) + 1]] <- a
+}
+
+names(post) <- c( "BM_human_H13","BM_human_H6","BM_human_H7", "BM_human_H9","BM_human_AP-B00182_",
+"BM_human_AP-B02149_" ,"BM_human_AP-B08041_", "BM_human_AP-B08805" , "BM_B000943" , "BM_B01320",      
+"BM_B02817" ,"BM_B10395")
+
+# Correlations
+for (i in 1:length(post)) {
+  seurat <- post[[i]]
+  b <- names(post)[i]
+  plots_list <- list()
+  
+  combinations <- list(
+    list("new_PC_markers_human", "Teff_Juanro_human", "PC vs Teffectoras"),
+    list("new_PC_markers_human", "TcellAct_Juanro_human", "PC vs Tactivadas"),
+    list("new_PC_markers_human", "genes38_human", "PC vs Texhausted"),
+    list("new_PC_markers_human", "cytotoxicity_human", "PC vs cytotoxicidad"),
+    list("genes38_human", "Teff_Juanro_human", "Texhausted vs Teffectoras"),
+    list("genes38_human", "TcellAct_Juanro_human", "Texhausted vs Tactivadas"),
+    list("genes38_human", "cytotoxicity_human", "Texhausted vs cytotoxicidad")
+  )
+  
+  for (comb in combinations) {
+    signature1 <- comb[[1]]
+    signature2 <- comb[[2]]
+    plot_title <- comb[[3]]
     
+    if (signature1 %in% names(seurat@meta.data) && signature2 %in% names(seurat@meta.data)) {
+      p <- ggplot(seurat@meta.data, aes(!!sym(signature1), !!sym(signature2))) +
+        geom_point() +
+        theme_cowplot() +
+        stat_cor(method = "spearman", cor.coef.name = "r") +
+        ggtitle(plot_title)
+      
+      plots_list <- c(plots_list, list(p))
+    } else {
+      message("Skipping combination ", signature1, " vs ", signature2, " for ", b)
+    }
+  }
+  num_plots <- length(plots_list)
+  height_per_plot <- 8.5  # Altura en pulgadas para cada gráfico en orientación horizontal
+  width_per_plot <- 11    # Ancho en pulgadas para cada gráfico en orientación horizontal
+  max_height <- 36        # Altura máxima en pulgadas para el PDF en orientación horizontal
+  
+  # Calcular la altura total manteniendo dentro del límite máximo
+  total_height <- min(num_plots * height_per_plot, max_height)
+  
+  # Combinar todos los gráficos y guardarlos como PDF en orientación horizontal
+  if (num_plots > 0) {
+    combined_plot <- ggarrange(plotlist = plots_list, ncol = 1, nrow = num_plots)
+    file_name_all_plots <- paste0("./results/human/correlation/plot/", b, "_all_plots.pdf")
+    ggsave(file_name_all_plots, combined_plot, width = width_per_plot, height = total_height, limitsize = FALSE)
+  } else {
+    message("No plots were created for ", b)
   }
   
 }
 
 
+## Check which genes are in each object
+resultados <- list()
 
+for (nombre_objeto in names(all)) {
+  genes_counts <- all[[nombre_objeto]]@assays[["RNA"]]@counts@Dimnames[[1]]
+  
+  for (nombre_firma in names(sig)) {
+    genes_interes <- sig[[nombre_firma]]
+    genes_ausentes <- genes_interes[!genes_interes %in% genes_counts]
+    resultados[[nombre_objeto]][[nombre_firma]] <- genes_ausentes
+  }
+}
 
-ggplot(subset(x = seurat@meta.data, name %in% c("M1_fem_1C","M2_F_2B","M8_F2_1C","M9_F2_1C")), 
-       aes(x=PC_new_UCell , y=Tcell_Exh_UCell)) + geom_point(mapping = aes(color = name)) + 
-  cowplot::theme_cowplot() + stat_cor(method = "spearman", cor.coef.name = "rho")
+# Convertir los resultados a un formato de tabla
+tabla_resultados <- do.call(rbind, lapply(resultados, function(x) {
+  sapply(x, function(y) paste(y, collapse = ", "))
+}))
+
+# Poner nombres a las filas y columnas
+rownames(tabla_resultados) <- names(resultados)
+colnames(tabla_resultados) <- names(sig)
+
+# Mostrar la tabla
+print(tabla_resultados)
+
 
 
