6beb576ab35ba21956b33b5c0877a4bdf26259fd
lsudupe <sudupelaura@gmail.com>
2023-12-04 09:58:53 +0300

    refactorization

--- graph_hotspots.R
+++ graph_hotspots.R
@@ -14,38 +14,23 @@ library(dbscan)
 ## Data
 mouse <- readRDS("./objects/sp/st_q3.rds")
 
-## RNA
-mouse <- NormalizeData(mouse, verbose = FALSE)
-mouse <- FindVariableFeatures(mouse, selection.method = "vst", nfeatures = 2000)
-mouse <- ScaleData(mouse, verbose = FALSE)
-
 # Define the subsets to processq
 subsets <- c("M1_fem_1C", "M2_F_2B", "M8_F2_1C", "M9_F2_1C")
 
-# Loop over each subset
-for (ident in subsets) {
-  # Subset the data
-  subset_mouse <- SubsetSTData(mouse, idents = c(ident))
-  
-}
+# Sample
+M8 <- SubsetSTData(mouse, idents = c("M8_F2_1C"))
+Idents(object = M8) <- M8@meta.data[["new_groups"]]
+M8 <- SubsetSTData(M8, idents = c("Hot_spot"))
 
-
-#####prueba
-
-M9 <- SubsetSTData(mouse, idents = c("M9_F2_1C"))
-Idents(object = M9) <- M9@meta.data[["new_groups"]]
-M9 <- SubsetSTData(M9, idents = c("Hot_spot"))
-coor <- M9@tools[["Staffli"]]@meta.data
-
-# Convert your spatial data to a matrix
-meta <- M9@meta.data
-coor <- M9@tools[["Staffli"]]@meta.data
+# Extract coordinates
+meta <- M8@meta.data
+coor <- M8@tools[["Staffli"]]@meta.data
 meta["x_coord"] <- as.vector(coor$x) 
 meta["y_coord"] <- as.vector(coor$y) 
 coordinates <- as.matrix(meta[, c("x_coord", "y_coord")])
 
-# Define the number of nearest neighbors (including the point itself)
-k <- 15  # This will find the 5 nearest neighbors
+# Define the number of nearest neighbors 
+k <- 15  
 
 # Find k nearest neighbors for each spot
 # The function returns a list of integer vectors, each containing the indices of the k nearest neighbors
@@ -71,7 +56,7 @@ for (i in 1:length(knn_result)) {
 graph <- simplify(graph)
 
 # Perform community detection using the Louvain algorithm
-communities <- cluster_louvain(graph,resolution =  0.6)
+communities <- cluster_louvain(graph,resolution =  0.2)
 
 # Add the community information to the metadata
 meta$community <- factor(communities$membership)
@@ -84,28 +69,35 @@ layout_matrix <- cbind(V(graph)$x, V(graph)$y)
 # Color the vertices based on the community membership to visualize clusters
 plot(communities, graph, layout=layout_matrix, vertex.size=3, vertex.label=NA, edge.arrow.size=.1, edge.width=0.1)
 
-
-prueba <- M9
-#Step 5: Extract info and add it to your spatial data
+# Extract info and add it to your spatial data
 community_membership <- communities$membership
 community_factor <- as.factor(community_membership)
 meta$community <- community_factor
-prueba <- AddMetaData(prueba, meta)
+M8 <- AddMetaData(M8, meta)
 
-FeatureOverlay(prueba, features = c("community"), 
+# Plot spatial
+FeatureOverlay(M8, features = c("community"), 
                sampleids = 1:6, ncols = 2, 
                pt.size = 1.3)
 
-M9 <- AddMetaData(M9, meta)
+
+# Change level names
 # k=15, res=0.2
 M8
+levels(M8@meta.data$community) <- c("S8_M1", "S8_M2")
 # k=15, res=0.5
 M2
+levels(M2@meta.data$community) <- c("S2_M1", "S2_M2", "S2_M3", "S2_M4")
 # k=15, res=0.2
 M1
+levels(M1@meta.data$community)[levels(M1@meta.data$community) == "1"] <- "S1_M1"
 # k=15, res=0.5
 M9
+levels(M9@meta.data$community) <- c("S9_M1", "S9_M2", "S9_M3")
 
-FeatureOverlay(M1, features = c("community"), 
-               sampleids = 1:6, ncols = 2, 
-               pt.size = 1.3)
+# Save data
+saveRDS(M1,"./objects/sp/st_s1_module.rds")
+saveRDS(M2,"./objects/sp/st_s2_module.rds")
+saveRDS(M8,"./objects/sp/st_s8_module.rds")
+saveRDS(M9, "./objects/sp/st_s9_module.rds")
+
