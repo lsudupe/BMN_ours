ffe6fd9f9f152f97c15cf0159eac88ee19a4449f
lsudupe <sudupelaura@gmail.com>
2024-02-14 15:54:01 +0300

    add Pg colors per group

--- colorBlind.R
+++ colorBlind.R
@@ -9,13 +9,23 @@ library(patchwork)
 
 distr_groups <- readRDS("./objects/sp/st_q3.rds")
 
-displayAvailablePalette()
+distr_groups@meta.data[["new_groups"]] <- as.factor(distr_groups@meta.data[["new_groups"]])
 
-# Access the SteppedSequential5Steps palette
-steppedSequentialPalette <- colorBlindness::SteppedSequential5Steps
+# Rename the levels
+levels(distr_groups@meta.data[["new_groups"]])[levels(distr_groups@meta.data[["new_groups"]]) == "Surronding"] <- "Border_zone"
+levels(distr_groups@meta.data[["new_groups"]])[levels(distr_groups@meta.data[["new_groups"]]) == "Low"] <- "Remote_zone"
+levels(distr_groups@meta.data[["new_groups"]])[levels(distr_groups@meta.data[["new_groups"]]) == "Hot_spot"] <- "Hotspot"
 
-# Extract specific colors
-selectedColors <- steppedSequentialPalette[c(3, 12, 18, 23)]
+# Check the updated unique values
+unique(distr_groups@meta.data[["new_groups"]])
+
+Idents(distr_groups) <- "new_groups"
+distr_groups_subset <- subset(distr_groups, subset = new_groups != "rest")
+distr_groups_subset@meta.data[["new_groups"]] <- droplevels(distr_groups_subset@meta.data[["new_groups"]])
+unique(distr_groups_subset@meta.data[["new_groups"]])
+
+selectedColors <- c(rest="#E5B17E", Remote_zone="#C3E57E", Border_zone="#7EC3E5", Hotspot="#CC5151",notknown="white")
+
 
 # Create a new color mapping for 'new_groups'
 # Assuming 'new_groups' is a factor with 4 levelsnr
@@ -32,7 +42,9 @@ dev.off()
 # Display the plot
 print(p)
 
-
+VlnPlot(distr_groups_subset, features = "Tnfrsf17", 
+                    group.by = "new_groups", cols = selectedColors,
+                    pt.size = 1) 
 
 
 my_list <- c("Tnfrsf17", "Plpp5", "Sdc1", "Fcrl5", "Slamf7", 
@@ -54,15 +66,16 @@ distr_groups <- mouse
 ####spatial plots
 distr_groups
 genes <- c("Sdc1", "Cd81", "Slamf7", "Prdm1", "Cd38", "Tnfrsf17", "Xbp1", "Mzb1", "Cd52")
-samples <- unique(distr_groups@meta.data$name)
-levels(distr_groups@meta.data$new_groups) <- c("Low", "rest", "Surrounding", "Hot_spot")
+samples <- unique(distr_groups_subset@meta.data$name)
+levels(distr_groups_subset@meta.data$new_groups) <- c("Remote_zone", "Border_zone", "Hotspot")
+selectedColors <- c(rest="#E5B17E", Remote_zone="#C3E57E", Border_zone="#7EC3E5", Hotspot="#CC5151",notknown="white")
 
 
 for (gen in genes) {
   # Verificar si el gen está en el objeto Seurat
-  if(gen %in% rownames(distr_groups@assays$SCT@data)) {
+  if(gen %in% rownames(distr_groups_subset@assays$SCT@data)) {
     # Calcula el valor máximo para el gen en todas las muestras
-    max_expression <- max(distr_groups@assays$SCT@data[gen, ])
+    max_expression <- max(distr_groups_subset@assays$SCT@data[gen, ])
     
     # Define el valor mínimo y máximo para la escala
     min_value <- 0
@@ -86,10 +99,12 @@ for (gen in genes) {
     
     for (sample in samples) {
       # Filter the Seurat object for the current sample
-      sample_data <- subset(distr_groups, subset = name == sample)
+      sample_data <- subset(distr_groups_subset, subset = name == sample)
       
       # Create a violin plot for the current gene and sample
-      vln_plot <- VlnPlot(sample_data, features = gen, group.by = "new_groups", pt.size = 1) + NoLegend() + ggtitle(sample)
+      vln_plot <- VlnPlot(sample_data, features = gen, 
+                          group.by = "new_groups", cols = selectedColors,
+                          pt.size = 1) + NoLegend() + ggtitle(sample)
       
       # Add the plot to the list
       sample_plots[[sample]] <- vln_plot
@@ -108,16 +123,24 @@ for (gen in genes) {
   }
 }
 
-
+M1 <- readRDS("./objects/sp/st_s1_community.rds")
+M2 <- readRDS("./objects/sp/st_s2_community.rds")
+M8 <- readRDS("./objects/sp/st_s8_community.rds")
+M9 <- readRDS("./objects/sp/st_s9_community.rds")
 
 ##communities
 samples <- c(M1,M2,M8,M9)
 names(samples) <- c("M1","M2","M8","M9")
 
 samples <- list(M1 = M1, M2 = M2, M8 = M8, M9 = M9)
+color_mapping <- c(Pg1="#000000", Pg2="#004949", Pg3="#009292", Pg4="#ff6db6", Pg5="#ffb6db",
+                   Pg6="#490092", Pg7="#006ddb", Pg8="#b66dff", Pg9="#6db6ff", Pg10="#b6dbff",
+                   Pg11="#920000", Pg12="#924900")
+
 
 genes <- c("Xbp1", "Mzb1", "Cd74", "Hsp90b1", "S100a9", "S100a8", "B2m", "Tmsb4x", "Sec11c", "Tpt1",
               "Cybb", "Fth1", "Cd52")
+genes <- c("Ighg2b", "Ighg3", "Cyba", "Ly6k", "S100a6")
 
 
 # Function to get max expression of a gene from a Seurat object
@@ -184,7 +207,8 @@ create_violin_plot <- function(gene, samples) {
   
   for (sample_name in names(samples)) {
     seurat_object <- samples[[sample_name]]
-    vln_plot <- VlnPlot(seurat_object, features = gene, group.by = "community", pt.size = 1) + NoLegend() + ggtitle(sample_name)
+    vln_plot <- VlnPlot(seurat_object, features = gene,  cols = color_mapping,
+                        group.by = "community", pt.size = 1) + NoLegend() + ggtitle(sample_name)
     plot_list[[sample_name]] <- vln_plot
   }
   
@@ -212,13 +236,16 @@ samples <- lapply(names(samples), function(x) {
   return(samples[[x]])
 })
 
+combined_samples@meta.data$community <- factor(combined_samples@meta.data$community, 
+                                               levels = c("Pg1", "Pg2", "Pg3", "Pg4", "Pg5", "Pg6", "Pg7", "Pg8", "Pg9", "Pg10", "Pg11", "Pg12"))
 # Combine all samples into one Seurat object
 combined_samples <- Reduce(function(x, y) merge(x, y), samples)
 
 # Create violin plots for each gene, separated by 'community'
 for (gene in genes) {
   if (gene %in% rownames(combined_samples@assays$SCT@data)) {
-    vln_plot <- VlnPlot(combined_samples, features = gene, group.by = "community", pt.size = 1) + NoLegend()
+    vln_plot <- VlnPlot(combined_samples, features = gene,  cols = color_mapping,
+                        group.by = "community", pt.size = 1) + NoLegend()
     
     # Save the violin plot to a PDF
     pdf_filename <- paste0("./results/genes_plots/hotspot/ALLviolin_", gene, "_community.pdf")
